3165cf2476d769fdb002a9513be8b52e9abc28f8
CodeScripterSD <jackson1781atminecraft@gmail.com>
2026-05-15 08:03:00 -0400

    Fix saturation issue

--- src/main/java/com/craftminerd/eunithice/item/ModFoods.java
+++ src/main/java/com/craftminerd/eunithice/item/ModFoods.java
@@ -5,7 +5,7 @@ import net.minecraft.world.food.FoodProperties;
 public class ModFoods {
     public static final FoodProperties MACROFIBER_APPLE = new FoodProperties.Builder()
             .nutrition(7)
-            .saturationModifier(1f)
+            .saturationModifier(0.2f)
             .fast()
             .build();
 }
